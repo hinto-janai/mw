17b4339d005196dd528e93cc9988e6a4ca31788d
hinto.janai <hinto.janai@protonmail.com>
2024-06-17 17:44:58 -0400

    add expected modules

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <div align="center">
 
-![CI](https://github.com/hinto-janai/mw/actions/workflows/ci.yml/badge.svg) [![crates.io](https://img.shields.io/crates/v/mw.svg)](https://crates.io/crates/mw) [![docs.rs](https://docs.rs/mw/badge.svg)](https://docs.rs/mw)
+![CI](https://github.com/hinto-janai/mw/actions/workflows/ci.yml/badge.svg)
 
 <!--
 <img src="https://github.com/hinto-janai/mw/assets/101352116/5c77d0aa-2f9b-4579-8d3c-b9e00e225179" width="60%"/>
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-use std::process::*;
+//! TODO
 
+// TODO: remove me.
+#![allow(unused_crate_dependencies)]
+
+//---------------------------------------------------------------------------------------------------- Mod
+mod book;
+mod cli;
+mod config;
+mod constants;
+mod disk;
+mod download;
+mod free;
+mod launch;
+mod panic;
+mod regex;
+mod statics;
+mod tui;
+mod update;
+
+//---------------------------------------------------------------------------------------------------- Use
+use std::process::{Command, Stdio};
+
+//---------------------------------------------------------------------------------------------------- Main
 fn main() {
     let mut cmd = Command::new("path/to/monero-wallet-cli")
         .stdin(Stdio::inherit())
